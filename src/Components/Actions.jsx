6b1eace1e26167b61fe4203d5fa40a1278d24763
unknown <MICHAELSIBANDA>
2025-04-23 17:51:43 +0200

    Quick update

--- src/Components/Actions.jsx
+++ src/Components/Actions.jsx
@@ -4,18 +4,10 @@ import "tippy.js/dist/tippy.css";
 const Actions = () => {
   return (
     <div className="hidden fixed bottom-10 right-10  w-[200px] min-h-[200px] md:flex flex-col items-center justify-end space-y-4">
-      <Tippy content="See my project">
-        <a
-          href="https://cinema-land.vercel.app"
-          target="_blank"
-          className="bg-[#C5FF41] hover:bg-[#6f9c05] text-black font-bold py-2 px-4 rounded">
-          Cinema Land
-        </a>
-      </Tippy>
-
+      
       <Tippy content="Check out my Github">
         <a
-          href="https://github.com/dumisa-sakhile"
+          href="https://github.com/MICHAELSIBANDA"
           target="_blank"
           className="bg-white hover:bg-gray-200 text-black font-bold py-2 px-10 rounded">
           Github
@@ -24,7 +16,7 @@ const Actions = () => {
 
       <Tippy content="Email me">
         <a
-          href="mailto:dumisa.message@outlook.com"
+          href="mailto:0740295373ms@gmail.com"
           className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-8 rounded">
           Email me
         </a>
