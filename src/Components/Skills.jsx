eddc7d82df1a5345e3637d1154def35e8cc76f47
unknown <MICHAELSIBANDA>
2025-04-23 17:57:40 +0200

    Quick update

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -93,68 +93,6 @@ const Skills = () => {
             />
           </a>
         </Tippy>
-
-        {/* tailwind */}
-        <Tippy content="TailwindCSS">
-          <a
-            href="https://tailwindcss.com/"
-            target="_blank"
-            className="hover:scale-105">
-            <img
-              src="https://www.svgrepo.com/show/354431/tailwindcss-icon.svg"
-              alt="tailwind"
-              className="w-10"
-            />
-          </a>
-        </Tippy>
-
-        {/* sass */}
-        <Tippy content="Syntactically Awesome Style Sheets">
-          <a
-            href="https://sass-lang.com/"
-            target="_blank"
-            className="hover:scale-105">
-            <img
-              src="https://www.svgrepo.com/show/374061/sass.svg"
-              alt="sass"
-              className="w-10"
-            />
-          </a>
-        </Tippy>
-      </div>
-
-      <br />
-
-      <p className="text-[#998F8F] text-lg md:w-[40%]">Backend Development:</p>
-      <br />
-      <div className="md:w-[40%] flex gap-2 items-start justify-start flex-wrap md:gap-6 *:bg-[#0b0808df]  *:p-4 *:rounded-lg *:ring-[1px] *:ring-[#353334]">
-        {/* node */}
-        <Tippy content="Node.js">
-          <a
-            href="https://nodejs.org/en/"
-            target="_blank"
-            className="hover:scale-105">
-            <img
-              src="https://www.svgrepo.com/show/303658/nodejs-1-logo.svg"
-              alt="node"
-              className="w-10"
-            />
-          </a>
-        </Tippy>
-
-        {/* express */}
-        <Tippy content="Express.js">
-          <a
-            href="https://expressjs.com/"
-            target="_blank"
-            className="hover:scale-105">
-            <img
-              src="https://www.svgrepo.com/show/376367/express.svg"
-              alt="express"
-              className="w-10"
-            />
-          </a>
-        </Tippy>
       </div>
 
       <br />
@@ -162,20 +100,6 @@ const Skills = () => {
       <p className="text-[#998F8F] text-lg md:w-[40%]">Databases:</p>
       <br />
       <div className="md:w-[40%] flex gap-2 items-start justify-start flex-wrap md:gap-6 *:bg-[#0b0808df]  *:p-4 *:rounded-lg *:ring-[1px] *:ring-[#353334]">
-        {/* mongodb */}
-        <Tippy content="MongoDB">
-          <a
-            href="https://www.mongodb.com/"
-            target="_blank"
-            className="hover:scale-105">
-            <img
-              src="https://www.svgrepo.com/show/439231/mongodb.svg"
-              alt="mongodb"
-              className="w-10"
-            />
-          </a>
-        </Tippy>
-
         {/* sql */}
         <Tippy content="SQL">
           <a
@@ -199,20 +123,6 @@ const Skills = () => {
             <img
               src="https://www.svgrepo.com/show/303251/mysql-logo.svg"
               alt="mysql"
-              className="w-10"
-            />
-          </a>
-        </Tippy>
-
-        {/* postgres */}
-        <Tippy content="PostgreSQL">
-          <a
-            href="https://www.postgresql.org/"
-            target="_blank"
-            className="hover:scale-105">
-            <img
-              src="https://www.svgrepo.com/show/354200/postgresql.svg"
-              alt="postgres"
               className="w-10"
             />
           </a>
